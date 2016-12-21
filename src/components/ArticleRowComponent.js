3768c38ce0287d019b076b036d8610357888ec88
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-12-21 15:50:36 -0500

    disable sorting on second click, make back button when showing author articles

--- src/components/ArticleRowComponent.js
+++ src/components/ArticleRowComponent.js
@@ -1,37 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function ArticleRow(props) {
+class ArticleRow extends Component {
 
-  let author = props.profile.first_name + " " + props.profile.last_name
+  fullName(){
+    return this.props.profile.first_name + " " + this.props.profile.last_name
+  }
 
-  let milliseconds = new Date() - new Date(props.publishAt),
+  sincePublish(){
+    let milliseconds = new Date() - new Date(this.props.publishAt),
       seconds = Math.floor(milliseconds / 1000 % 60),
       minutes = Math.floor(milliseconds / 1000 / 60 % 60),
       hours = Math.floor(milliseconds / 1000 / 60 / 60)
+    let unit, value
+    if (hours > 0) {
+      value = hours
+      unit = "hour"
+    } else if (minutes > 0) {
+      value = minutes
+      unit = "minute"
+    } else {
+      value = seconds
+      unit = "second"
+    }
+    return value > 1 ? `${value} ${unit}s ago` : `${value} ${unit} ago`
 
-  let unit, value
-  if (hours > 0) {
-    value = hours
-    unit = "hour"
-  } else if (minutes > 0) {
-    value = minutes
-    unit = "minute"
-  } else {
-    value = seconds
-    unit = "second"
   }
 
-  let sincePublish = value > 1 ? `${value} ${unit}s ago` : `${value} ${unit} ago`
+  handleAuthorClick(event) {
+    debugger
 
-  return (
-    <tr>
-      <td><img width="50px" height="50px" src={props.image}/></td>
-      <td><a href={props.url}>{props.title}</a></td>
-      <td>{author}</td>
-      <td className="words">{props.words}</td>
-      <td className="publish_at">{sincePublish}</td>
-    </tr>
-  );
+    this.props.getAuthorArticles(event.target.innerHTML)
+  }
+
+  render(){
+    return (
+      <tr>
+        <td><img width="50px" height="50px" src={this.props.image}/></td>
+        <td><a href={this.props.url}>{this.props.title}</a></td>
+        <td><a href="javascript:" onClick={this.handleAuthorClick.bind(this)}>{this.fullName()}</a></td>
+        <td className="words">{this.props.words}</td>
+        <td className="publish_at">{this.sincePublish()}</td>
+      </tr>
+    );
+  }
 }
 
 export default ArticleRow;
--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -6,29 +6,43 @@ class Table extends Component {
 
   constructor(){
     super()
-    this.state = {sortBy: null, pages: 1, articles: []}
+    this.state = {sortBy: null, pages: 1, articles: [], authorArticles: []}
+
+    // bind user event functions
     this.showTenMore = this.showTenMore.bind(this)
     this.sortByPublished = this.sortByPublished.bind(this)
     this.sortByWords = this.sortByWords.bind(this)
+    this.goBack = this.goBack.bind(this)
   }
 
   componentWillMount(){
+    // get stored sort preference
+    this.setState({sortBy: window.localStorage.getItem("PubArt_SORTBY_PREF")})
+    // Request original 30 articles
     this.loadMain()
   }
 
   showing() {
-    return this.state.articles.slice(0, this.state.pages * 10)
+    // Access only the first (pages * 10) articles, called in sort()
+    if (this.state.authorArticles.length > 0) {
+      return this.state.authorArticles
+    } else {
+      return this.state.articles.slice(0, this.state.pages * 10)
+    }
   }
 
   showTenMore(){
+    // If already showing all stored articles, get more articles first
     if (this.showing().length === this.state.articles.length) {
       this.loadMore()
     }
+    // Increment number of showing "pages"
     this.setState({pages: this.state.pages + 1})
-    this.sort()
   }
 
   loadMain(){
+    // see loadMore()
+
     $.ajax({
       context: this,
       url: "more-articles.json",
@@ -41,6 +55,12 @@ class Table extends Component {
   }
 
   loadMore(){
+    // typically we'd have a backend for articles, and send along a page number
+    // backend would send back the next n articles
+    // "next n" refers either to the n articles most recently entered into the database
+    //   or the most recent n based on the publish_at attribute
+    //   (if these conditions aren't equivalent)
+
     $.ajax({
       context: this,
       url: "articles.json",
@@ -54,6 +74,7 @@ class Table extends Component {
 
   sort(){
     let type = this.state.sortBy
+    window.localStorage.setItem("PubArt_SORTBY_PREF", type)
     return this.showing().sort((a, b)=>{
       if (a[type] > b[type]) {
         return -1
@@ -66,26 +87,51 @@ class Table extends Component {
   }
 
   sortByPublished(){
-    this.setState({sortBy: "publish_at"})
-    this.sort()
+    // handle sorting via sortBy attribute of the Table component's state
+    // setState cause React to check if component needs to re-render
+    //    render relies on the changed attribute sortBy via this.sort()
+    this.state.sortBy === "publish_at" ?
+      this.setState({sortBy: "null"}) : this.setState({sortBy: "publish_at"})
   }
 
   sortByWords(){
-    this.setState({sortBy: "words"})
-    this.sort()
+    this.state.sortBy === "words" ?
+      this.setState({sortBy: "null"}) : this.setState({sortBy: "words"})
+  }
+
+  getAuthorArticles(fullName){
+    // currently filters articles stored locally to show only those with the name of
+    //    the clicked author
+    // With a backend, this would instead just be a database query to get all of the author's
+    //   unpublished articles, since we probably aren't looking for just the ones we
+    //   currently have stored
+    // With an author/profile model, this would go to 'profiles/:id/articles'
+
+    let authorArticles = this.state.articles.filter((article)=>{
+      return article.profile.first_name + " " + article.profile.last_name === fullName
+    })
+    this.setState({authorArticles: authorArticles})
+  }
+
+  goBack(){
+    this.setState({authorArticles: []})
+  }
+
+  isAuthorShowing(){
+    return this.state.authorArticles.length !== 0
   }
 
   render() {
     let articleRows = this.sort().map((article, index)=>{
       return(
-        <ArticleRow key={`article${index}`} title={article.title} profile={article.profile} words={article.words} publishAt={article.publish_at} url={article.url} image={article.image}/>
+        <ArticleRow key={`article${index}`} title={article.title} profile={article.profile} words={article.words} publishAt={article.publish_at} url={article.url} image={article.image} getAuthorArticles={this.getAuthorArticles.bind(this)}/>
       )
     })
 
     return (
       <table>
         <thead><tr>
-          <th colSpan="2" className="title" > UNPUBLISHED ARTICLES (60) </th>
+          <th colSpan="2" className="title" > UNPUBLISHED ARTICLES ({this.showing().length}) </th>
           <th>AUTHOR</th>
           <th className="words" onClick={this.sortByWords}>
             WORDS
@@ -100,7 +146,7 @@ class Table extends Component {
           {articleRows}
         </tbody>
         <tfoot><tr>
-          <td colSpan="5"><button hidden={this.state.pages === 6} onClick={this.showTenMore}>Load more articles</button></td>
+          <td colSpan="5"><button hidden={this.state.pages === 6 && !this.isAuthorShowing()} onClick={this.isAuthorShowing() ? this.goBack : this.showTenMore}>{this.isAuthorShowing() ? "Go back" : "Load more articles"}</button></td>
         </tr></tfoot>
       </table>
     );
